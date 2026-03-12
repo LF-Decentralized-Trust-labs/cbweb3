a48255ffbca9b900f9dc9f2f1a079d55bad4f726
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2026-03-12 17:32:17 -0300

    chore: Makefile adjustments in commands and new entries

--- platform/make/contracts.mk
+++ platform/make/contracts.mk
@@ -1,7 +1,7 @@
 include contracts/.env
 
 contracts.fmt:
-	@cd contracts && FOUNDRY_PROFILE=${FOUNDRY_PROFILE} forge fmt --check
+	@cd contracts && FOUNDRY_PROFILE=${FOUNDRY_PROFILE} forge fmt
 
 contracts.lint:
 	@cd contracts && FOUNDRY_PROFILE=${FOUNDRY_PROFILE} forge lint
@@ -24,10 +24,15 @@ contracts.gen-doc:
 contracts.serve-doc:
 	@cd contracts && forge doc --serve
 
-contracts.deploy-besu:
+contracts.deploy-tcebm-besu:
 	@cd contracts && FOUNDRY_PROFILE=${FOUNDRY_PROFILE} forge script script/TokenizedCentralBankMoney.s.sol:DeployTCeBM --rpc-url ${BESU_RPC_URL} --broadcast
+
+contracts.deploy-htlc-besu:
+	@cd contracts && FOUNDRY_PROFILE=${FOUNDRY_PROFILE} forge script script/HashTimeLockedContract.s.sol:DeployHTLC --rpc-url ${BESU_RPC_URL} --broadcast
 
 contracts.slither:
 	@docker run --rm -v $(CURDIR)/contracts:/share -w /share trailofbits/eth-security-toolbox slither . --config-file slither.config.json
 
-.PHONY: contracts.fmt contracts.lint contracts.test contracts.coverage contracts.build contracts.clean contracts.gen-doc contracts.serve-doc contracts.deploy-besu contracts.slither
+contracts.full-check: contracts.build contracts.fmt contracts.lint contracts.test contracts.coverage contracts.slither
+
+.PHONY: contracts.fmt contracts.lint contracts.test contracts.coverage contracts.build contracts.clean contracts.gen-doc contracts.serve-doc contracts.deploy-tcebm-besu contracts.deploy-htlc-besu contracts.slither contracts.full-check
